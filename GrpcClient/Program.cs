b5b430e34e919fd9931c5c2c6059c5f3c0612e99
Stephen <stefanodinucci@gmail.com>
2021-01-01 23:03:35 +0100

    Creato nuovo servizio Client
    Aggiunto servizio client, chiamata, e ritorni

---
+++ GrpcClient/Program.cs
@@ -0,0 +1,36 @@
+﻿using Grpc.Net.Client;
+using GrpcServer;
+using System;
+using System.Threading.Tasks;
+
+namespace GrpcClient
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            //var input = new HelloRequest { Name = "Step" };
+
+            //var channel = GrpcChannel.ForAddress("https://localhost:5001");
+            //var client = new Greeter.GreeterClient(channel);
+
+            //var reply = await client.SayHelloAsync(input);
+
+            //Console.WriteLine(reply.Message);
+
+
+            var channel = GrpcChannel.ForAddress("https://localhost:5001");
+            var clientClient = new Client.ClientClient(channel);
+
+            for (int i = 0; i < 5; i++)
+            {
+                var clientRequested = new ClientLookupModel { Id = i };
+
+                var reply = await clientClient.GetClientInfoAsync(clientRequested);
+
+                Console.WriteLine($"Age:{reply.Age}\n FirstName:{reply.FirstName}\n LastName:{reply.LastName}\n IsItWarner?:{reply.IsIt}");
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ GrpcServer/Services/ClientsService.cs
@@ -0,0 +1,47 @@
+﻿using Grpc.Core;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GrpcServer.Services
+{
+    public class ClientsService : Client.ClientBase
+    {
+        private readonly ILogger<ClientsService> _logger;
+
+        public ClientsService(ILogger<ClientsService> logger)
+        {
+            _logger = logger;
+        }
+
+        public override Task<ClientModel> GetClientInfo(ClientLookupModel request, ServerCallContext context)
+        {
+            ClientModel output = new ClientModel();
+
+            if (request.Id == 1)
+            {
+                output.Age = 142;
+                output.FirstName = "Egmonte";
+                output.LastName = "Natale";
+            }
+            else if(request.Id == 3)
+            {
+                output.Age = 14;
+                output.FirstName = "Wakko";
+                output.LastName = "Warner";
+                output.IsIt = true;
+            }
+            else if( request.Id == 2)
+            {
+                output.Age = 10;
+                output.FirstName = "Dot";
+                output.LastName = "Princess Angelina Contessa Louisa Francesca Banana Fanna Bo Besca III";
+                output.IsIt = true;
+            }
+
+            return Task.FromResult(output);
+        }
+    }
+}
